57898810cd0a6483a0a386df8847181741c1d29b
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-07-04 16:16:12 +0300

    second commit and test fix go get -v

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,49 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"unicode"
+)
+
+type wordsCount struct {
+	word  string
+	count int
+}
+
+func Top10(s string) []string {
+	freq := make(map[string]int)
+
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c) && !unicode.IsPunct(c)
+	}
+	a := strings.FieldsFunc(s, f)
+	for _, c := range a {
+		freq[c]++
+	}
+
+	wcList := make([]wordsCount, len(freq))
+	i := 0
+	for key, val := range freq {
+		wcList[i] = wordsCount{key, val}
+		i++
+	}
+
+	sort.Slice(wcList, func(i, j int) bool {
+		return wcList[i].count > wcList[j].count
+	})
+
+	// fmt.Println(wcList)
+	result := make([]string, 0)
+	for i, v := range wcList {
+		// fmt.Println(result, v.word, ":", v.count) # for debug
+		if i < 10 {
+			result = append(result, v.word)
+		} else {
+			break
+		}
+	}
+	fmt.Println(result)
+	return result
 }
