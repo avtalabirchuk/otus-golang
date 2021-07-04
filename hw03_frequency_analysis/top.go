50191591c36f14828d4d8c2bb467ba817ca31549
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-07-04 12:14:21 +0300

    first test

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -12,6 +12,12 @@ type wordsCount struct {
 	count int
 }
 
+type PairList []wordsCount
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].count < p[j].count }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
 func Top10(s string) []string {
 	// s = strings.ToLower(s)
 	freq := make(map[string]int)
@@ -24,16 +30,17 @@ func Top10(s string) []string {
 		freq[c]++
 	}
 
-	wcList := make([]wordsCount, len(freq))
+	wcList := make(PairList, len(freq))
 	i := 0
 	for key, val := range freq {
 		wcList[i] = wordsCount{key, val}
 		i++
 	}
+	sort.Sort(sort.Reverse(wcList))
 
-	sort.Slice(wcList, func(i, j int) bool {
-		return wcList[i].count > wcList[j].count
-	})
+	// sort.Slice(wcList, func(i, j int) bool {
+	// 	return wcList[i].count > wcList[j].count
+	// })
 
 	fmt.Println(wcList)
 	result := make([]string, 0)
