8f2ad7b26f3b5dc61190ba2ee53996d247a2d96f
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-07-15 16:35:30 +0300

    add cache part2 HW

---
+++ hw04_lru_cache/cache.go
@@ -0,0 +1,65 @@
+package hw04lrucache
+
+type Key string
+
+type Cache interface {
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу.
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу.
+	Clear()                              // Очистить кэш.
+}
+
+type lruCache struct {
+	capacity int               // емкость
+	queue    List              // очередь последних элементов
+	items    map[Key]*ListItem // словарь
+}
+
+// Задать значение ключа  и положить его в кеш.
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	item, keyExists := lru.items[key]
+	// Проверка на существование ключа в кеше, если есть то переместить на верх списка
+	if keyExists {
+		item.Value = cacheItem{Key: key, Value: value}
+		lru.queue.MoveToFront(item)
+	} else {
+		// Если нет, содается новый элемент очереди и помещается в начало списка
+		item = lru.queue.PushFront(cacheItem{Key: key, Value: value})
+		lru.items[key] = item
+		// если длинна очереди становится больше емкости, последний элемент очереди удаляется
+		if lru.queue.Len() > lru.capacity {
+			lastItem := lru.queue.Back()
+			lru.queue.Remove(lastItem)
+			delete(lru.items, Key(lastItem.Value.(cacheItem).Key))
+		}
+	}
+	return keyExists
+}
+
+// Получаем значение из кэша.
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	item, keyExist := lru.items[key]
+	if keyExist {
+		lru.queue.MoveToFront(item)
+		return item.Value.(cacheItem).Value, true
+	}
+	// ничего не нашли
+	return nil, false
+}
+
+func (lru *lruCache) Clear() {
+	lru.items = make(map[Key]*ListItem)
+	lru.queue = &list{}
+}
+
+type cacheItem struct {
+	Key   Key
+	Value interface{}
+}
+
+func NewCache(capacity int) Cache {
+	return &lruCache{
+		capacity: capacity,
+		items:    make(map[Key]*ListItem),
+		queue:    &list{},
+	}
+}
