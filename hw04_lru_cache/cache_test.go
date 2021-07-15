9b1270a68a7a9def2e08bc907ed98020b4b01db4
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-07-15 16:35:59 +0300

    add unit test for cache

---
+++ hw04_lru_cache/cache_test.go
@@ -0,0 +1,118 @@
+package hw04lrucache
+
+import (
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCache(t *testing.T) {
+	t.Run("empty cache", func(t *testing.T) {
+		c := NewCache(10)
+
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
+
+	t.Run("simple", func(t *testing.T) {
+		c := NewCache(5)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("purge logic", func(t *testing.T) {
+		c := NewCache(5)
+		c.Set("aaa", 200)
+		c.Set("bbb", 200) // [bbb: 200, aaa: 200]
+		c.Clear()
+		c.Set("ccc", 100) // [ccc: 100]
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+		_, ok = c.Get("ccc")
+		require.True(t, ok)
+	})
+
+	t.Run("capacity logic", func(t *testing.T) {
+		c := NewCache(2)
+		c.Set("aaa", 200) // [aaa: 200]
+		c.Set("bbb", 200) // [bbb: 200, aaa: 200]
+		c.Set("ccc", 200) // [ccc: 200, bbb: 200]
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
+		_, ok = c.Get("bbb")
+		require.True(t, ok)
+		_, ok = c.Get("ccc")
+		require.True(t, ok)
+	})
+
+	t.Run("ejection logic", func(t *testing.T) {
+		c := NewCache(3)
+		c.Set("aaa", 200)
+		c.Set("bbb", 200)
+		c.Set("ccc", 200) // [ccc: 200, bbb: 200, aaa 200]
+		c.Get("aaa")      // [aaa: 200, ccc: 200, bbb: 200]
+		c.Get("bbb")      // [bbb: 200, aaa: 200, ccc: 200]
+		c.Set("ddd", 200) // [ddd: 200, bbb: 200, aaa: 200]
+		_, ok := c.Get("aaa")
+		require.True(t, ok)
+		_, ok = c.Get("ccc")
+		require.False(t, ok)
+		_, ok = c.Get("bbb")
+		require.True(t, ok)
+	})
+}
+
+func TestCacheMultithreading(t *testing.T) {
+	t.Skip() // Remove me if task with asterisk completed.
+
+	c := NewCache(10)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		}
+	}()
+
+	wg.Wait()
+}
