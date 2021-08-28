c011b3ecc63d6e308f72072f4e9b63f738b85f79
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-08-28 19:46:23 +0300

    add hw07 first condition

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,49 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type test struct {
+	limit         int64
+	offset        int64
+	checkFileName string
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	for _, tst := range []test{
+		{
+			limit:         0,
+			offset:        0,
+			checkFileName: "testdata/out_offset0_limit0.txt",
+		},
+	} {
+		t.Run(fmt.Sprintf("Limit: %d, Offset: %d", tst.limit, tst.offset), func(t *testing.T) {
+			targetFile, err := ioutil.TempFile("/tmp/", "output")
+			require.NoError(t, err)
+			defer os.Remove(targetFile.Name())
+
+			err = Copy("testdata/input.txt", targetFile.Name(), tst.offset, tst.limit)
+			require.NoError(t, err)
+
+			checkFile, _ := os.Open(tst.checkFileName)
+			defer checkFile.Close()
+
+			checkFileContent, err := ioutil.ReadAll(checkFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			targetFileContent, err := ioutil.ReadAll(targetFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			require.Equal(t, checkFileContent, targetFileContent)
+		})
+	}
 }
