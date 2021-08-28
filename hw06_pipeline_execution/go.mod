61676ceff115372bbf3a38abf1a73db4369ec115
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-08-28 11:49:36 +0300

    add HW parallel execution

--- hw06_pipeline_execution/go.mod
+++ hw06_pipeline_execution/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw06_pipeline_execution
+module github.com/avtalabirchuk/otus-golang/hw06_pipeline_execution
 
 go 1.16
 
--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,37 @@ type (
 
 type Stage func(in In) (out Out)
 
+// in -> st1 -> done_stage -> st2 -> done_stage -> st3 -> done_stage -> out
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := doneStage(in, done)
+	for _, stage := range stages {
+		out = doneStage(stage(out), done)
+	}
+
+	return doneStage(out, done)
+}
+
+func doneStage(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case d, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- d:
+				case <-done:
+					return
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
 }
