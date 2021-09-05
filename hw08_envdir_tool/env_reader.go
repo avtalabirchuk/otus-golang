0d323c985d6e274a80c7bd2e42094a8a25e1d8cd
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-09-05 15:31:20 +0300

    add reader from file simple

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,58 @@
 package main
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
-type EnvValue struct {
-	Value      string
-	NeedRemove bool
-}
+// type EnvValue struct {
+// 	Value      string
+// 	NeedRemove bool
+// }
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
+func readFirstLineFile(file *os.File) (string, error) {
+	reader := bufio.NewReader(file)
+	value, _ := reader.ReadString('\n')
+
+	return value, nil
+}
+
+func processValue(value string) string {
+	value = strings.TrimRight(value, " \t\n")
+	value = strings.ReplaceAll(value, "\x00", "\n")
+	return value
+}
+
+func getValueFromFileDir(dir, fileName string) (string, error) {
+	filePath := path.Join(dir, fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	value, _ := readFirstLineFile(file)
+	value = processValue(value)
+
+	return value, nil
+}
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
-	return nil, nil
+	fileNames, _ := ioutil.ReadDir(dir)
+
+	env := make(Environment, len(fileNames))
+	for _, fileName := range fileNames {
+		value, _ := getValueFromFileDir(dir, fileName.Name())
+		env[fileName.Name()] = value
+	}
+	return env, nil
+
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	// programName := os.Args[0]
+
+	// if len(os.Args) < 3 {
+	// 	log.Fatalf("Program shud run as: %s <path to directory with env> <command> <some arg>", programName)
+	// }
+
+	envDir := os.Args[1]
+	commandAndArg := os.Args[2:]
+
+	env, _ := ReadDir(envDir)
+	// if err != nil {
+	// 	log.Fatalf("Unable to get environment variables info: %v", err)
+	// }
+	code := RunCmd(commandAndArg, env)
+	os.Exit(code)
 }
