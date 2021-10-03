0f0a09c65ec946f4cb8d8d8f66b93c0983217107
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-03 09:54:58 +0300

    add several condition on task, and setup base telnet cli

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,74 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var durTime = 10
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	if len(os.Args) < 3 {
+		log.Fatal("Please use: telnet host port [--timeout=2s]")
+	}
+	timeout := flag.Duration("timeout", time.Duration(durTime), "use it to specify dial timeout")
+	flag.Parse()
+	log.Println(os.Args[1:])
+	host := os.Args[2]
+	port := os.Args[3]
+	address := net.JoinHostPort(host, port)
+	log.Println(address)
+	client := NewTelnetClient(
+		address,
+		*timeout,
+		os.Stdin,
+		os.Stdout)
+	if err := client.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Connect to %v...", address)
+	sigintChannel := make(chan os.Signal, 1)
+
+	signal.Notify(sigintChannel, syscall.SIGINT)
+
+	go func() {
+		<-sigintChannel
+		fmt.Println("Got SIGINT")
+		err := client.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		for {
+			err := client.Receive()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+		}
+		wg.Done()
+	}()
+	go func() {
+		for {
+			err := client.Send()
+			if err != nil {
+				log.Println(err)
+				break
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -11,10 +14,66 @@ type TelnetClient interface {
 	Send() error
 	Receive() error
 }
+type TCPClient struct {
+	address string
+	timeout time.Duration
+
+	inReader *bufio.Reader
+	out      io.Writer
+
+	conn       net.Conn
+	connReader *bufio.Reader
+}
+
+func (tc *TCPClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", tc.address, tc.timeout)
+	if err != nil {
+		return err
+	}
+	tc.conn = conn
+	tc.connReader = bufio.NewReader(conn)
+	return nil
+}
+
+func (tc *TCPClient) Close() error {
+	if err := tc.conn.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tc *TCPClient) Send() error {
+	data, err := tc.inReader.ReadBytes('\n')
+	if err != nil {
+		return err
+	}
+	_, err = tc.conn.Write(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tc *TCPClient) Receive() error {
+	data, connErr := tc.connReader.ReadBytes('\n')
+	_, outWriteErr := tc.out.Write(data)
+	if connErr != nil {
+		return fmt.Errorf("error receive data: %w", connErr)
+	}
+	if outWriteErr != nil {
+		return fmt.Errorf("error outwrited data: %w", outWriteErr)
+	}
+	return nil
+}
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	// Place your code here.
-	return nil
+	return &TCPClient{
+		address:  address,
+		timeout:  timeout,
+		inReader: bufio.NewReader(in),
+		out:      out,
+	}
 }
 
 // Place your code here.
