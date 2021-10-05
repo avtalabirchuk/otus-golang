5b4ebcb0da52976a10f0296ff21d875265572571
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 21:51:54 +0300

    test xatrg kill 1

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -14,7 +14,7 @@ TL_PID=$!
 
 sleep 5
 #ps a | grep '127.0.0.1'
-ps a | grep '127.0.0.1' | awk '{print $1}' > /tmp/pl
+ps aux | grep '127.0.0.1' | awk '{print $2}' > /tmp/pl
 cat /tmp/pl | xargs kill -s kill || true
 # kill ${TL_PID} 2>/dev/null || true
 # kill ${NC_PID} 2>/dev/null || true
