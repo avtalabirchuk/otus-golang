275466c22ee7fac2c12b1e1f23911372c3af1854
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 20:49:07 +0300

    test shell sh

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -3,21 +3,18 @@ set -xeuo pipefail
 
 go build -o go-telnet
 
-(echo -e "Hello" && cat 2>/dev/null) | nc -l 127.0.0.1 4242 >./nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
 NC_PID=$!
-ip a
 
 sleep 5
-(echo -e "I" && cat 2>/dev/null) | ./go-telnet --timeout=5s 127.0.0.1 4242 >./telnet.out &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/tmp/telnet.out &
 TL_PID=$!
-ip a
 
 sleep 5
 kill ${TL_PID} 2>/dev/null || true
 kill ${NC_PID} 2>/dev/null || true
-cat ./telnet.out
-cat ./nc.out
-
+cat /tmp/nc.out
+cat /tmp/telnet.out
 
 function fileEquals() {
   local fileData
@@ -25,11 +22,15 @@ function fileEquals() {
   [ "${fileData}" = "${2}" ] || (echo -e "unexpected output, $1:\n${fileData}" && exit 1)
 }
 
-expected_nc_out='I'
-fileEquals ./nc.out "${expected_nc_out}"
+expected_nc_out='I
+am
+TELNET client'
+fileEquals /tmp/nc.out "${expected_nc_out}"
 
-expected_telnet_out='Hello'
-fileEquals ./telnet.out "${expected_telnet_out}"
+expected_telnet_out='Hello
+From
+NC'
+fileEquals /tmp/telnet.out "${expected_telnet_out}"
 
 rm -f go-telnet
 echo "PASS"
