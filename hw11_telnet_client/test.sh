5a143df2d44c4166b2569039c2aca1ba296cbab5
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 21:11:15 +0300

    test cat telnet1

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -15,7 +15,7 @@ TL_PID=$!
 sleep 5
 kill ${TL_PID} 2>/dev/null || true
 kill ${NC_PID} 2>/dev/null || true
-#cat $NC 
+cat $NC 
 cat $TELNET
 echo "path=$NC data=$(cat $NC)"
 echo "path=$TELNET data=$(cat $TELNET)"
