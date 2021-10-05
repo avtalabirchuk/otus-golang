d5ef40f4a1d016609fe67f3fb1cdc05b5d3782e7
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 21:07:43 +0300

    test shell script

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -15,8 +15,9 @@ TL_PID=$!
 sleep 5
 kill ${TL_PID} 2>/dev/null || true
 kill ${NC_PID} 2>/dev/null || true
-cat $NC
-cat $TELNET
+echo "path=$NC data=$(cat $NC)"
+echo "path=$TELNET data=$(cat $TELNET)"
+sleep 1
 
 function fileEquals() {
   local fileData
