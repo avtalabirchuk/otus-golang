ff46f492f9dbc2b87ad2eace108b1bd91ae74c15
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 21:03:09 +0300

    test sh vars

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
+NC=/tmp/nc.out
+TELNET=/tmp/telnet.out
 
 go build -o go-telnet
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >$NC &
 NC_PID=$!
 
 sleep 5
-(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >/tmp/telnet.out &
+(echo -e "I\nam\nTELNET client\n" && cat 2>/dev/null) | ./go-telnet --timeout=5s localhost 4242 >$TELNET &
 TL_PID=$!
 
 sleep 5
 kill ${TL_PID} 2>/dev/null || true
 kill ${NC_PID} 2>/dev/null || true
-sleep 1
+cat $NC
+cat $TELNET
 
 function fileEquals() {
   local fileData
@@ -25,12 +28,12 @@ function fileEquals() {
 expected_nc_out='I
 am
 TELNET client'
-fileEquals /tmp/nc.out "${expected_nc_out}"
+fileEquals $NC "${expected_nc_out}"
 
 expected_telnet_out='Hello
 From
 NC'
-fileEquals /tmp/telnet.out "${expected_telnet_out}"
+fileEquals $TELNET "${expected_telnet_out}"
 
 rm -f go-telnet
 echo "PASS"
