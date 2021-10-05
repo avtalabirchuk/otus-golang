60c3fcaf11bbca491ef60c6dfc753d9c826348ca
Talabirchuk Andrey <andrey.talabirchuk@x5.ru>
2021-10-05 20:46:39 +0300

    test shell

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -11,12 +11,13 @@ sleep 5
 (echo -e "I" && cat 2>/dev/null) | ./go-telnet --timeout=5s 127.0.0.1 4242 >./telnet.out &
 TL_PID=$!
 ip a
-cat ./telnet.out
-cat ./nc.out
 
 sleep 5
 kill ${TL_PID} 2>/dev/null || true
 kill ${NC_PID} 2>/dev/null || true
+cat ./telnet.out
+cat ./nc.out
+
 
 function fileEquals() {
   local fileData
